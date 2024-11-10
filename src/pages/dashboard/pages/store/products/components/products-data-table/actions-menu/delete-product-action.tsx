c136da9f6effd294def2cc45e865dae7077f9350
Fabian Andres Parra Sanchez <andeveling@gmail.com>
2024-11-10 11:57:56 -0500

    Update product delete alert

--- src/pages/dashboard/pages/store/products/components/products-data-table/actions-menu/delete-product-action.tsx
+++ src/pages/dashboard/pages/store/products/components/products-data-table/actions-menu/delete-product-action.tsx
@@ -1,28 +1,59 @@
-import { DropdownMenuItem } from '@/components/ui/dropdown-menu';
-import { Trash, Loader } from 'lucide-react';
-import { Product } from '@/models/product.model';
-import { useDeleteProduct } from '../../../hooks/use-delete-product-hook';
 import { Button } from '@/components';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { DropdownMenuItem } from '@/components/ui/dropdown-menu';
+import { Product } from '@/models/product.model';
+import { Trash } from 'lucide-react';
+import { useState } from 'react';
+import { Fragment } from 'react/jsx-runtime';
+import { useDeleteProduct } from '../../../hooks/use-delete-product-hook';
 
 export const DeleteProductAction = ({ product }: { product: Product }) => {
-  const { mutate, isPending } = useDeleteProduct(product.id);
-
+  const { mutate } = useDeleteProduct(product.id);
+  const [isOpen, setIsOpen] = useState(false);
   const handleDelete = () => mutate();
+
   return (
-    <DropdownMenuItem asChild>
-      <Button
-        variant="ghost"
-        size="sm"
-        onClick={handleDelete}
-        disabled={isPending}
-      >
-        {isPending ? (
-          <Loader className="mr-2 h-4 w-4" />
-        ) : (
-          <Trash className="mr-2 h-4 w-4" />
-        )}
-        Delete product
-      </Button>
-    </DropdownMenuItem>
+    <Fragment>
+      <DropdownMenuItem asChild>
+        <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+          <AlertDialogTrigger asChild>
+            <Button variant="ghost" size="sm">
+              <Trash className="h-4 w-4" />
+              Delete product
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Delete product</AlertDialogTitle>
+              <AlertDialogDescription>
+                Are you sure you want to delete this product?
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel asChild>
+                <Button variant="outline" size="sm">
+                  Cancel
+                </Button>
+              </AlertDialogCancel>
+              <AlertDialogAction asChild>
+                <Button variant="destructive" size="sm" onClick={handleDelete}>
+                  Delete
+                </Button>
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </DropdownMenuItem>
+    </Fragment>
   );
 };
